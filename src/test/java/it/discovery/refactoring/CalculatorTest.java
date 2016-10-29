353d610c2218ea4e8e5fecadd5ef39b16c93005c
Мегабит <@ITDiscovery-12>
2016-10-29 17:04:23 +0300

    test

--- src/test/java/it/discovery/refactoring/CalculatorTest.java
+++ src/test/java/it/discovery/refactoring/CalculatorTest.java
@@ -1,12 +1,35 @@
 package it.discovery.refactoring;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class MainClassTest {
+public class CalculatorTest {
 	
 	@Test
-	public void test_() {
-		System.out.println("OK!");
+	//Given_When_Then
+	public void calculate_SumWithDecimalScaleAndFileOutput_Success() {
+		assertEquals(0, Calculator.calculate("+", "2", "3", "10", "false", ""));
+	}
+	
+	@Test
+	//Given_When_Then
+	public void calculate_SumWithDecimalScaleAndConsuleOutput_Success() {
+		int result = Calculator.calculate("+", "1", "2", "10", "true", "");
+		assertEquals(0, result);
+	}
+	
+	@Test
+	//Given_When_Then
+		public void calculate_SumFirstArgumentNonNumber_Error() {
+		int result = Calculator.calculate("+", "aaa", "3", "10", "true", "");
+		assertEquals(-1, result);
+	}
+	
+	@Test
+	//Given_When_Then
+		public void calculate_SumSecondArgumentNonNumber_Error() {
+		int result = Calculator.calculate("+", "2", "bb", "10", "true", "");
+		assertEquals(-1, result);
 	}
 
 }
