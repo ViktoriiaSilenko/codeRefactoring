2694f2ba3b020540857cc1c3c325d35b29b749f6
Мегабит <@ITDiscovery-12>
2016-10-30 16:05:59 +0200

    removed unused imports

--- src/main/java/it/discovery/refactoring/CalculationScanner.java
+++ src/main/java/it/discovery/refactoring/CalculationScanner.java
@@ -1,8 +1,6 @@
 package it.discovery.refactoring;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
